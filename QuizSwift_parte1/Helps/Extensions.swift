77515adf4163ecfab0c81e4a939447358c57803d
sergio <sergiot1805@gmail.com>
2023-11-22 13:34:17 +0100

    Initial Commit

---
+++ QuizSwift_parte1/Helps/Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Extensions.swift
+//  QuizSwift_parte1
+//
+//  Created by c142 DIT UPM on 22/11/23.
+//
+
+import Foundation
+
+
+infix operator =+-= : ComparisonPrecedence
+
+extension String {
+    static func =+-= (s1:String, s2:String) -> Bool {
+        var a = s1.lowercased().trimmingCharacters(in:.whitespaces)
+        var b = s2.lowercased().trimmingCharacters(in:.whitespaces)
+        return a == b
+    }
+}
+
+
+/*
+extension String: LocalizedError {
+    public var errorDescription: String? {
+        return self
+    }
+}*/
---
+++ QuizSwift_parte1/QuizSwift_parte1App.swift
@@ -0,0 +1,23 @@
+//
+//  QuizSwift_parte1App.swift
+//  QuizSwift_parte1
+//
+//  Created by c142 DIT UPM on 22/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuizSwift_parte1App: App {
+    @State var quizzesModel = QuizzesModel()
+    @State var scoresModel = ScoresModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(quizzesModel)
+                .environment(scoresModel)
+        }
+    }
+}
