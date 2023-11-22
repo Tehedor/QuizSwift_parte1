d42d3a36ce4da29d65c540ad3ff5062e693833d3
sergio <sergiot1805@gmail.com>
2023-11-22 13:50:48 +0100

    añadido todos los views

---
+++ QuizSwift_parte1/Views/QuizItemPlayView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizItemPlayView.swift
+//  QuizSwift_parte1
+//
+//  Created by c142 DIT UPM on 22/11/23.
+//
+
+import SwiftUI
+
+struct QuizItemPlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizItemPlayView()
+}
---
+++ QuizSwift_parte1/Views/QuizListView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizListView.swift
+//  QuizSwift_parte1
+//
+//  Created by c142 DIT UPM on 22/11/23.
+//
+
+import SwiftUI
+
+struct QuizListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizListView()
+}
